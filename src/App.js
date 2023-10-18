35a879ba2c457f55b0146bebca518bd419e037ec
Doug <108644233+DougFaveroAnjos@users.noreply.github.com>
2023-10-18 16:45:52 -0300

    Adicionado campos de texto Nome, Cargo e Imagem

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import Banner from './componentes/Banner/Banner';
+import Banner from './componentes/Banner';
+import CampoTexto from './componentes/CampoTexto';
+
+
 
 function App() {
   return (
     <div className="App">
       <Banner />      
+      <CampoTexto placeholder='Digite seu Nome' label='Nome'/>
+      <CampoTexto placeholder='Digite seu Cargo' label='Cargo'/>
+      <CampoTexto placeholder='Insira a URL da sua imagem' label='Imagem'/>
     </div>
   );
 }
---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,12 @@
+import './CampoTexto.css'
+
+export function CampoTexto(props){
+    return(
+        <div className='campo-texto'>
+            <label>{props.label}</label>
+            <input placeholder={props.placeholder}/>
+        </div>
+    )
+}
+
+export default CampoTexto
